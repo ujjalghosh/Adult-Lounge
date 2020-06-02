7094c9fa6d107275c91f6a3d6abc8ff92f83617b
Ujjal Ghosh <ghosh.ujjal27@gmail.com>
2020-06-03 00:22:40 +0530

    admin error fix

--- application/views/admin/add_show_type.php
+++ application/views/admin/add_show_type.php
@@ -1,5 +1,5 @@
-<?php $this->load->view('admin/layout/header'); ?>
-<?php $this->load->view('admin/layout/sidemenu'); ?>
+<?php $this->load->view('admin/layout/header');?>
+<?php $this->load->view('admin/layout/sidemenu');?>
 <div class="content">
     <div class="content-header">
         <div class="leftside-content-header">
@@ -11,29 +11,29 @@
     </div>
     <div class="row animated fadeInUp">
         <div class="col-sm-12 col-md-12">
-            <?php if($this->session->flashdata('success_msg')){ ?>
+            <?php if ($this->session->flashdata('success_msg')) {?>
             <!--<div class="alert alert-success fade in">
     <a href="#" class="close" data-dismiss="alert">×</a>
     <h4><i class="icon fa fa-check"></i> Success!</h4>
     <?=$this->session->flashdata('success_msg')?>
 </div>-->
-            <?php } ?>
+            <?php }?>
             <div class="panel">
                 <div class="panel-content">
                     <div class="row">
                         <div class="col-md-12">
                             <form action="<?=base_url('admin/show-type')?>" class="form-horizontal" method="post">
-                                <input type="hidden" name="show_edit_id" value="<?php if(isset($show)){ print $show[0]['id']; }?>" />
+                                <input type="hidden" name="show_edit_id" value="<?php if (isset($show)) {print $show[0]['id'];} else {echo '0';}?>" />
                                 <div class="form-group">
                                     <label for="email2" class="col-sm-4 control-label">Show Type Name *</label>
                                     <div class="col-sm-4">
-                                        <input type="text" name="showname" class="form-control commonRequired" value="<?php if(isset($show)){ print $show[0]['name']; }?>" required>
+                                        <input type="text" name="showname" class="form-control commonRequired" value="<?php if (isset($show)) {print $show[0]['name'];}?>" required>
                                         <span class="help-block"></span>
                                     </div>
                                 </div>
                                 <div class="form-group">
                                     <div class="col-sm-offset-4 col-sm-8">
-                                        <button type="submit" class="btn btn-wide btn-primary form-submit-button"><?php if($mode == 'Add'){ ?>Save<?php }else{ ?>Save Changes<?php } ?> </button>
+                                        <button type="submit" class="btn btn-wide btn-primary form-submit-button"><?php if ($mode == 'Add') {?>Save<?php } else {?>Save Changes<?php }?> </button>
                                         <a href="<?=base_url('admin/show-type')?>"><button type="button" class="btn btn-danger">Cancel</button></a>
                                     </div>
                                 </div>
@@ -45,4 +45,4 @@
         </div>
     </div>
 </div>
-<?php $this->load->view('admin/layout/footer'); ?>
+<?php $this->load->view('admin/layout/footer');?>
